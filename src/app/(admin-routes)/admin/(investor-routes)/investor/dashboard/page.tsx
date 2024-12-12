3397fcd5028bef62857969dd3f127e28b075de0b
gmcb <gmendescamargodebarros@gmail.com>
2024-12-12 16:53:27 -0300

    feat: add logout feature

--- src/app/(admin-routes)/admin/(investor-routes)/investor/dashboard/page.tsx
+++ src/app/(admin-routes)/admin/(investor-routes)/investor/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
+import ButtonLogout from "@/components/ButtonLogout";
+import { getServerSession } from "next-auth";
 import React from "react";
 
-export default function DashboardPage() {
-  return <h1>bem vindo investidor!</h1>
+export default async function DashboardPage() {
+  const session = await getServerSession(nextAuthOptions);
+
+  return (
+    <>
+      <h1>bem vindo investidor {session?.name} !</h1>
+      <ButtonLogout />
+    </>
+  )
 }
