1418b4ead251a0b747503d3e63983aec72b4dcec
gmcb <gmendescamargodebarros@gmail.com>
2024-12-12 19:35:06 -0300

    feat: update the signin to compare password hashed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/lib/prisma"
 import NextAuth, { NextAuthOptions } from "next-auth"
+import bcrypt from 'bcrypt';
 import CredentialsProvider from "next-auth/providers/credentials"
 
 const nextAuthOptions: NextAuthOptions = {
@@ -11,14 +12,18 @@ const nextAuthOptions: NextAuthOptions = {
 				password: { label: 'password', type: 'password' }
 			},
 
-			async authorize(credentials, req) {
+			async authorize(credentials) {
+				if (!credentials) {
+					throw new Error('Invalid credentials');
+				}
+
         const user = await db.user.findUniqueOrThrow({
           select: {
             email: true,
             id: true,
             image: true,
             name: true,
-            password: false,
+            password: true,
             createdAt: true,
             updatedAt: true,
 						UserRole: {
@@ -34,14 +39,25 @@ const nextAuthOptions: NextAuthOptions = {
 						},
           },
           where: {
-            email: credentials?.email,
-            password: credentials?.password
-          },
-					
+            email: credentials.email,
+          }
         })
 
-				return user
-			},
+				const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+        if (!isPasswordValid) {
+          throw new Error('Invalid email or password');
+        }
+
+				return {
+					id: user.id,
+					email: user.email,
+					image: user.image,
+					name: user.name,
+					createdAt: user.createdAt,
+					updatedAt: user.updatedAt,
+					UserRole: user.UserRole
+				}
+			}
 		})
 	],
 	pages: {
